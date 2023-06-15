34637195b701baa76b5b71d28028866d06dbed2b
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2023-06-15 11:25:08 +0300

    Time: 43 ms (61.98%), Space: 16.3 MB (53.48%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n in [0,1,2]:
+            return n
+        else:
+            x=1
+            y=2
+            for i in range(2,n):
+                ai=x+y
+                x=y
+                y=ai
+            return ai
+            
+        
