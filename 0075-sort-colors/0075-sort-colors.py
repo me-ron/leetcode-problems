6a641f091cfabb80249515cf318e2912f7885b31
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2023-08-03 23:27:48 +0300

    Time: 42 ms (86.87%), Space: 16.3 MB (72.03%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -7,4 +7,10 @@ class Solution:
             for j in range (i+1,len(nums)):
                 if nums[i]>nums[j]:
                     nums[i],nums[j]=nums[j],nums[i]
+                    
+                
+            
+        
+            
+            
         
