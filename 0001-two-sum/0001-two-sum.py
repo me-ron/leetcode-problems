89bbf1a28aee926fd702e57dc3d56063c1c15cbe
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2023-11-17 21:36:52 +0300

    Time: 69 ms (51.46%), Space: 17.7 MB (9.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         a=0
+        num={n:idx for idx,n in enumerate(nums)}
         for i in range(len(nums)):
             p = target-nums[i]
-            if p in nums:
-                a=nums.index(p)
+            if p in num:
+                a=num[p]
                 if a==i:
                     continue
                 break
